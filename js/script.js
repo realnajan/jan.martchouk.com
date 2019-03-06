eae8f4ef659f8d4d26068327aedb9c3677b0626f
phrafox <jan@martchouk.com>
2019-03-06 16:16:27 +0100

    More Designs + Modals

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
 $(function () {
 
-  var x = window.matchMedia("(min-width: 992px)");
-  if (x.matches) {
-};
+  var pageWidth = $(window).width();
+  if (pageWidth > 1023) {
+    $(".design").click(function() {
+      var target = $(this).data("modal")
+      $("#" + target).css("display", "block")
+    })
+
+    $(".img-modal").click(function() {
+      id = $(this).attr('id')
+      $("#"+id).css("display", "none")
+    }).children().click(function(e) {
+      return false
+    })
+
+    $(".img-mclose").click(function() {
+      $(this).parent().parent().css("display", "none")
+    })
+  }
 });
