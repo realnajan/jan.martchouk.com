d2d71dc802638890d39efae5cf52442ed35826f6
phrafox <jan@martchouk.com>
2019-06-11 10:32:16 +0200

    BG Fix

--- js/script_home.js
+++ js/script_home.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
   links.push("polaroid");
   var randlink = links[Math.floor(Math.random() * links.length)];
   console.log(randlink);
-  console.log('url(file:///home/jan/retro.jan.martchouk.com/img/assets/bgs/'+randlink+'.jpg)')
+  console.log('url(https://realnajan.github.io/jan.martchouk.com/img/assets/bgs/'+randlink+'.jpg)')
   $('body').css('background', 'linear-gradient(90deg, rgba(0, 0, 0, 0.9), rgba(112, 66, 20, 0.5) ), url(file:///home/jan/retro.jan.martchouk.com/img/assets/bgs/'+randlink+'.jpg)');
 
 });
