f7770fa9688ed8c2b5b7d062f01b0688c6c78b2a
phrafox <jan@martchouk.com>
2019-06-11 10:56:28 +0200

    try again with bg

--- js/script_home.js
+++ js/script_home.js
@@ -9,6 +9,4 @@ $(document).ready(function() {
   console.log(randlink);
   console.log('url(https://realnajan.github.io/jan.martchouk.com/img/assets/bgs/'+randlink+'.jpg)')
   $('body').css('background', 'linear-gradient(90deg, rgba(0, 0, 0, 0.9), rgba(112, 66, 20, 0.5) ), url(https://realnajan.github.io/jan.martchouk.com/img/assets/bgs/'+randlink+'.jpg)');
-  $('body').css('background', 'linear-gradient(90deg, rgba(0, 0, 0, 0.9), rgba(112, 66, 20, 0.5) ), url(file:///home/jan/retro.jan.martchouk.com/img/assets/bgs/'+randlink+'.jpg)');
-
 });
