db87ae6b751e4548da3a8925b6e2d5b6b3be873c
najan <jan@martchouk.com>
2020-07-30 17:42:56 +0200

    Updates

---
+++ precache-manifest.18389d1029b9df72b6a3dd3bc5dfa1f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d8da8f876d1bf1cea6fb29a0a0f815b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bb1fc04be3c75778ddc",
+    "url": "/static/css/main.f7286c0f.chunk.css"
+  },
+  {
+    "revision": "0b47085c0c84f46d37ab",
+    "url": "/static/js/2.1d4ec095.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1d4ec095.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bb1fc04be3c75778ddc",
+    "url": "/static/js/main.d3d2554b.chunk.js"
+  },
+  {
+    "revision": "45da4c18aad61a4aa654",
+    "url": "/static/js/runtime-main.70a08211.js"
+  }
+]);
